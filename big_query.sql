8ec9cd7a50aa7e5111fa801e43da473412715b01
HHassan10120 <hhassan10120@gmail.com>
2021-09-21 19:38:58 +0100

    commit

--- big_query.sql
+++ big_query.sql
@@ -1,8 +1,8 @@
---Query for finding different different freight percentages
-SELECT sum(a.price) as sum_price, b.product_category_name, sum(a.freight_value) as sum_freight_v, 
+--query for finding different different freight percentages
+select sum(a.price) as sum_price, b.product_category_name, sum(a.freight_value) as sum_freight_v, 
 sum(a.freight_value) / sum(a.price) as freight_percentage
- FROM `encoded-metrics-150222.kaggle_brazilian_ecommerce.olist_order_items_dataset` a
- LEFT JOIN  `encoded-metrics-150222.kaggle_brazilian_ecommerce.olist_products_dataset` b
- ON a.product_id = b.product_id
+ from `encoded-metrics-150222.kaggle_brazilian_ecommerce.olist_order_items_dataset` a
+ left join  `encoded-metrics-150222.kaggle_brazilian_ecommerce.olist_products_dataset` b
+ on a.product_id = b.product_id
  group by b.product_category_name
- order by freight_percentage desc 
+ order by freight_percentage desc
